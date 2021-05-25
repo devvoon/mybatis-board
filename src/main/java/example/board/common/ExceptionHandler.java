e31ed2022bf621759f5f0f216d41eca544ba90c2
dawoonjeong <iamdawoonjeong@gmail.com>
2021-05-25 16:03:44 +0900

    add error

---
+++ src/main/java/example/board/common/ExceptionHandler.java
@@ -0,0 +1,21 @@
+package example.board.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionHandler {
+    private Logger log = LoggerFactory.getLogger(ExceptionHandler.class);
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception){
+        ModelAndView mv = new ModelAndView("/error/error_default");
+        mv.addObject("exception", exception);
+        log.error("exception", exception);
+        return mv;
+    }
+}
