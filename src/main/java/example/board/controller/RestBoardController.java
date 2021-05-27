35d961e12db3d5b2f7578f1e46591464c3f2638b
dawoonjeong <iamdawoonjeong@gmail.com>
2021-05-27 15:27:15 +0900

    add restapi

--- src/main/java/example/board/controller/RestBoardController.java
+++ src/main/java/example/board/controller/RestBoardController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,56 +21,57 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class BoardController {
+public class RestBoardController {
 
     private final BoardService boardService;
 
-    @RequestMapping("/board/openBoardList")
+    @RequestMapping(value="/board", method = RequestMethod.GET)
     public ModelAndView openBoardList() throws Exception{
-        ModelAndView mv = new ModelAndView("board/boardList");//호출되는 html 화면
-        List<BoardDto> list = boardService.selectBoardList();
+        ModelAndView mv = new ModelAndView("board/restBoardList");
+        List<BoardDto> list=  boardService.selectBoardList();
+        System.out.println(">>>>>>>>>>>>>>>>>>>>list:" + list.size());
         mv.addObject("list", list);
         return mv;
     }
 
-    @RequestMapping("/board/openBoardWrite")
+    @RequestMapping(value="/board/write", method=RequestMethod.GET)
     public String openBoardWrite() throws Exception{
-        return "/board/boardWrite";
+        return "/board/restBoardWrite";
     }
 
-    @RequestMapping("/board/insertBoard")
-    public String insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception{
-        boardService.insertBoard(board, multipartHttpServletRequest);
-        return "redirect:/board/openBoardList";
+    @RequestMapping(value="/board/write", method=RequestMethod.POST)
+    public String insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpRequest) throws Exception{
+        boardService.insertBoard(board, multipartHttpRequest);
+        return "redirect:/board";
     }
 
-    @RequestMapping("/board/openBoardDetail")
-    public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception{
-        ModelAndView mv =new ModelAndView("board/boardDetail");
+    @RequestMapping(value="/board/{boardIdx}", method=RequestMethod.GET)
+    public ModelAndView openBoardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception{
+        ModelAndView mv = new ModelAndView("board/restBoardDetail");
 
         BoardDto board = boardService.selectBoardDetail(boardIdx);
         mv.addObject("board", board);
+
         return mv;
     }
 
-    @RequestMapping("/board/updateBoard")
+    @RequestMapping(value="/board/{boardIdx}", method=RequestMethod.PUT)
     public String updateBoard(BoardDto board) throws Exception{
         boardService.updateBoard(board);
-        return "redirect:/board/openBoardList";
+        return "redirect:/board";
     }
 
-    @RequestMapping("/board/deleteBoard")
-    public String deleteBoard(int boardIdx) throws Exception{
+    @RequestMapping(value="/board/{boardIdx}", method=RequestMethod.DELETE)
+    public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception{
         boardService.deleteBoard(boardIdx);
-        return "redirect:/board/openBoardList";
+        return " redirect:/board";
     }
 
-    @RequestMapping("/board/downloadBoardFile")
-    public void downloadBoardFile(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception{
+    @RequestMapping(value="/board/file", method = RequestMethod.GET)
+    public void downloadBoardFileRes(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception{
         BoardFileDto boardFile = boardService.selectBoardFileInformation(idx, boardIdx);
-        if(ObjectUtils.isEmpty(boardFile) == false){
+        if(false == ObjectUtils.isEmpty(boardFile)){
             String fileName = boardFile.getOriginalFileName();
-
             byte[] files = FileUtils.readFileToByteArray(new File(boardFile.getStoredFilePath()));
 
             response.setContentType("application/octet-stream");
@@ -79,8 +82,8 @@ public class BoardController {
             response.getOutputStream().write(files);
             response.getOutputStream().flush();
             response.getOutputStream().close();
-
         }
     }
+
 
 }
